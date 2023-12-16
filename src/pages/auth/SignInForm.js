baf9f1d7d6ac4f2c64697dbd5bea656fb08fd8e7
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-12-16 19:42:11 +0000

    Add useContext to the nav

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
@@ -11,8 +11,10 @@ import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import axios from "axios";
+import { SetCurrentUserContext } from "../../App";
 
 function SignInForm() {
+  const setCurrentUser = useContext(SetCurrentUserContext)
 //   Add your component logic here
    const [signInData, setSignInData] = useState({
     username: '',
@@ -32,7 +34,8 @@ function SignInForm() {
    const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("/dj-rest-auth/login/", signInData);
+      const {data} = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user)
       history.push("/");
     } catch (err) {
         setErrors(err.response?.data);
