4c8f29ddc079832df0d7fe43a73b77cf624266d4
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-12-19 06:06:11 +0000

    Toggle burger menu outside navbar click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,12 +7,27 @@ import { NavLink } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from 'axios';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 
 const NavBar = () => {
   // const currentUser = useContext(CurrentUserContext) replaced by the below code
   const currentUser = useCurrentUser()
+  // const [expanded, setExpanded] = useState(false);
+  // const ref = useRef(null)
+  // useEffect(() => {
+  //   const handleClickOutside = (event) => {
+  //     if (ref.current && ref.current.contains(event.target)){
+  //       setExpanded(false)
+  //     }
+  //   }
+  //   document.addEventListener('mouseup', handleClickOutside)
+  //   return () => {
+  //     document.removeEventListener('mouseup', handleClickOutside)
+  //   }
+  // }, [ref])     (moved to useClickOutsideToggle.js)
 
+  const {expanded, setExpanded, ref } = useClickOutsideToggle()
   const setCurrentUser = useSetCurrentUser()
 
   const handleSignOut = async () => {
@@ -81,7 +96,7 @@ const NavBar = () => {
                       </NavLink>
                     </> 
     return (
-      <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Navbar expanded= {expanded} className={styles.NavBar} expand="md" fixed="top">
         <Container>
           <NavLink to='/'>
             <Navbar.Brand>
@@ -89,7 +104,7 @@ const NavBar = () => {
             </Navbar.Brand>
           </NavLink>
           {currentUser && addPostIcon}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle ref= {ref} onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
               <NavLink exact to='/' className={styles.NavLink} activeClassName={styles.Active}>
