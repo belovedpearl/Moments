e5f7087cc3974daae89708b85eaa8905e2b9a2cf
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-12-27 10:17:38 +0000

    Add ProfileDataContext

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,43 @@
+import { useContext,createContext, useState, useEffect } from "react";
+import { useCurrentUser } from "./CurrentUserContext";
+import { axiosReq } from "../api/axiosDefaults";
+
+const ProfileDataContext = createContext();
+const SetProfileDataContext = createContext();
+
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+ 
+export const ProfileDataProvider = ({children}) => {
+  const [profileData, setProfileData] = useState({
+    pageProfile: {results: []},
+    popularProfiles: {results: []}
+  })
+  // const {popularProfiles} = profileData
+  const currentUser = useCurrentUser()
+
+  useEffect(() => {
+    const handleMount = async () => {
+        try{
+           const {data} = await axiosReq.get(
+            "/profiles/?ordering=-followers_count"
+           )
+           setProfileData(prevState => ({
+            ...prevState,
+            popularProfiles: data,
+                   }))
+        } catch (err){
+            console.log(err)
+        }
+    }
+    handleMount()
+  }, [currentUser])
+  return (
+    <ProfileDataContext.Provider value={profileData}>
+      <SetProfileDataContext.Provider value={setProfileData}>
+        {children}
+      </SetProfileDataContext.Provider>
+    </ProfileDataContext.Provider>
+) 
+}
--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,35 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import appStyles from '../../App.module.css'
 import Container from "react-bootstrap/Container";
-import { axiosReq } from '../../api/axiosDefaults';
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Asset from '../../components/Asset';
 import Profile from './Profile';
+import { useProfileData } from '../../contexts/ProfileDataContext';
 
 const PopularProfiles = ({mobile}) => {
-  const [profileData, setProfileData] = useState({
-    pageProfile: {results: []},
-    popularProfiles: {results: []}
-  })
-  const {popularProfiles} = profileData
-  const currentUser = useCurrentUser()
+  // const [profileData, setProfileData] = useState({
+  //   pageProfile: {results: []},
+  //   popularProfiles: {results: []}
+  // })
+  // const {popularProfiles} = profileData
+  // const currentUser = useCurrentUser()
 
-  useEffect(() => {
-    const handleMount = async () => {
-        try{
-           const {data} = await axiosReq.get(
-            "/profiles/?ordering=-followers_count"
-           )
-           setProfileData(prevState => ({
-            ...prevState,
-            popularProfiles: data,
-                   }))
-        } catch (err){
-            console.log(err)
-        }
-    }
-    handleMount()
-  }, [currentUser])
+  // useEffect(() => {
+  //   const handleMount = async () => {
+  //       try{
+  //          const {data} = await axiosReq.get(
+  //           "/profiles/?ordering=-followers_count"
+  //          )
+  //          setProfileData(prevState => ({
+  //           ...prevState,
+  //           popularProfiles: data,
+  //                  }))
+  //       } catch (err){
+  //           console.log(err)
+  //       }
+  //   }
+  //   handleMount()
+  // }, [currentUser]) replaced with prompts from ProfileDataContexts below
+
+  const { popularProfiles } = useProfileData();
+
   return (
     <Container className={`${appStyles.Content} ${
       mobile && "d-lg-none text-center mb-3"
