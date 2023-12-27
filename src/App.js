a3a1b52482fd3a15b1b3d2b93d48a0b38919be10
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-12-27 18:42:30 +0000

    Set up the profile header

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostPage from './pages/posts/PostPages';
 import PostsPage from './pages/posts/PostsPage';
 import { useCurrentUser } from './contexts/CurrentUserContext';
 import PostEditForm from './pages/posts/PostEditForm';
+import ProfilePage from './pages/profiles/ProfilePage';
 
 // import { createContext, useEffect, useState } from 'react';
 // import axios from 'axios';
@@ -71,6 +72,7 @@ function App() {
                 <Route exact path='/posts/create' render={ () => <PostCreateForm/> } />
                 <Route exact path="/posts/:id" render={() => <PostPage />} />
                 <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+                <Route exact path= "/profiles/:id" render={() => <ProfilePage />} />
                 <Route render={ () => <p>Page not Found!</p> }/>
               </Switch>
               
