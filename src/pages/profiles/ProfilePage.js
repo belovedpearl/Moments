be4027fa9b83eb4cc116b781da036adff52b1e08
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-12-28 06:36:49 +0000

    Add edit profile functions

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -24,6 +24,8 @@ import Post from "../posts/Post";
 import { fetchMoreData } from "../../utils/utils";
 import NoResults from "../../assets/no-results.png";
 
+import { ProfileEditDropdown } from "../../components/MoreDropdown";
+
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
@@ -59,6 +61,7 @@ function ProfilePage() {
 
   const mainProfile = (
     <>
+      {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
           {/* <p>Image</p> Replaced with bootstrap image component below  */}
